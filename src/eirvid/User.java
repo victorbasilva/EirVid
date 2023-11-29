a38d9461fb31fae09ee1df00d3b6df442f8cbd60
victorbasilva <102388646+victorbasilva@users.noreply.github.com>
2023-11-29 17:49:01 +0000

    Initialization
    Initial main and classes creation

---
+++ src/eirvid/User.java
@@ -0,0 +1,14 @@
+package eirvid;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 
+ */
+public class User {
+    
+}
