028d85e2cc9a9a2d9351c7d10fe406b36ae73e2a
victorbasilva <102388646+victorbasilva@users.noreply.github.com>
2023-12-07 14:10:01 +0000

    Main project developing. Movies source creation (.csv).

--- src/eirvid/EirVid.java
+++ src/eirvid/EirVid.java
@@ -2,7 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
+
 package eirvid;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -14,8 +23,25 @@ public class EirVid {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        //Movies options and prices source
+        
+        String fileSelected = "C:\\Users\\victo\\OneDrive\\Documents\\NetBeansProjects\\EirVid\\src\\eirvid\\Resource\\Filmes.csv"; 
+        StringBuilder strBuilderResponse = new StringBuilder();
+
+        File file = new File(fileSelected);
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8")); 
+                String sCurrentLine;
+                while ((sCurrentLine = br.readLine()) != null) {
+                    System.out.println(sCurrentLine);
+                    strBuilderResponse.append(sCurrentLine);
+                    strBuilderResponse.append(System.lineSeparator());
+                }
+            } catch (IOException e) {
+        }
     }
     
 }
