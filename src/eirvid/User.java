b619882b5754a2f4eb88383730a6f99536d968f8
Joelma Rodrigues <113507468+joelmarodrigues@users.noreply.github.com>
2023-12-07 20:14:36 +0000

    Update User.java

--- src/eirvid/User.java
+++ src/eirvid/User.java
@@ -1,14 +1,25 @@
 package eirvid;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
- *
- * @author 
+ *Joelma Rodrigues
+ * 2023246
  */
 public class User {
-    
+    private String name;
+    private String surname;
+    private String email;
+
+    public String getName(){
+        return name;
+    }
+    public void setSurname(){
+        public void setSurname(String surname){
+            this.surname = surname;
+        }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
 }
