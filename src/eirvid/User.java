327182484d1817bb6aec9d593162b0ff394ecfb8
Joelma Rodrigues <113507468+joelmarodrigues@users.noreply.github.com>
2023-12-10 13:35:56 +0000

    Update User.java
    - missing setter name
    - missing get surname

--- src/eirvid/User.java
+++ src/eirvid/User.java
@@ -1,19 +1,27 @@
 package eirvid;
 
 /**
- *Joelma Rodrigues
- * 2023246
+ * Student Name:Joelma Rodrigues
+ * Student ID: 2023246
  */
+
 public class User {
     private String name;
     private String surname;
     private String email;
 
+    //Contructor for User missing
+
     public String getName(){
         return name;
     }
-    public void setSurname(){
-        public void setSurname(String surname){
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getSurname(){
+            return surname;
+        }
+    public void setSurname(String surname){
             this.surname = surname;
         }
     public String getEmail(){
