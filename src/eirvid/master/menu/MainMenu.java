8d07d01b9e49d2318b28137baac5b8e833172352
rafael30121987 <114699259+rafael30121987@users.noreply.github.com>
2023-12-14 11:50:12 +0000

    Update MainMenu.java

--- src/eirvid/master/menu/MainMenu.java
+++ src/eirvid/master/menu/MainMenu.java
@@ -1,5 +1,6 @@
 package eirvid.master.menu;
 
+import java.util.Scanner;
 
 /* 
 * author: Rafael 
@@ -7,6 +8,37 @@ package eirvid.master.menu;
 */
 
 public class MainMenu {
+
+   public void Menu() {
+    
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the EirVid!");
+        System.out.println("1 - Login");
+        System.out.println("2 - Logout");
+
+        int start = sc.nextInt();  // Read user input
+
+        if (start == 1) {
+            System.out.println("The choice is: " + start);
+        } else if (start == 2) {
+            System.out.println("The choice is: " + start);
+        }
+        
+        System.out.println("Access the System");
+        
+        Scanner loginUser = new Scanner(System.in);
+        System.out.println("Enter your email");
+        String email = loginUser.nextLine();  // Read user input
+
+        System.out.println("Your email is: " + email);
+        
+        Scanner loginPassword = new Scanner(System.in);        
+        System.out.println("Enter your password");
+        String password = loginPassword.nextLine();  // Read user input
+
+        System.out.println("Your password is: " + password);
+
+    }
 
    
 }
